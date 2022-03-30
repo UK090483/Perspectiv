068784cb6baf99c74dd6994a4cd68a7c5e28522c
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-03-30 11:32:05 +0200

    init

--- components/Blocks/ListingBlock/Listings/Testimonials/TestimonialList.tsx
+++ components/Blocks/ListingBlock/Listings/Testimonials/TestimonialList.tsx
@@ -5,6 +5,8 @@ import TestimonialListItem from "./TestimonialListItem";
 // import useInViewport from "@hooks/useInViewport";
 import useKeyPress from "@hooks/useKeyPress";
 import useCounter from "@hooks/useCounter";
+import { Navigation } from "./Navigation";
+import { Dots } from "./Dots";
 
 interface ITestimonialListProps {
   items: TestimonialItemResult[];
@@ -35,7 +37,7 @@ const TestimonialList: React.FunctionComponent<ITestimonialListProps> = (
         tabIndex={0}
         {...keyPressHandler}
         ref={ref}
-        // onClick={next}
+        onClick={next}
         // onFocus={stop}
         // onBlur={start}
         // onMouseEnter={stop}
@@ -80,63 +82,3 @@ const TestimonialList: React.FunctionComponent<ITestimonialListProps> = (
 };
 
 export default TestimonialList;
-
-type DotsProps = {
-  count: number;
-  active: number;
-  onChange: (number: number) => void;
-  className?: string;
-};
-
-const Dots: React.FC<DotsProps> = (props) => {
-  const { count = 0, active = 0, onChange, className } = props;
-  if (count < 2) return <></>;
-  return (
-    <div className={` flex justify-center items-center py-4  ${className}`}>
-      {new Array(count).fill("a").map((i, index) => (
-        <div
-          onClick={() => onChange(index)}
-          key={index}
-          className={`w-3 h-3 mx-0.5 rounded-full  border-[0.5px] transition-colors border-black ${
-            active === index ? "bg-black" : " bg-white"
-          }`}
-        />
-      ))}
-    </div>
-  );
-};
-
-type NavigationProps = {
-  onNext?: () => void;
-  onPrev?: () => void;
-  className?: string;
-  count?: number;
-};
-
-const Navigation: React.FC<NavigationProps> = (props) => {
-  const {
-    children,
-    onNext = () => {},
-    onPrev = () => {},
-    className,
-    count = 0,
-  } = props;
-  if (count < 2) return <></>;
-  return (
-    <div className={` w-full flex  justify-between  ${className}`}>
-      <button
-        className=" m-2 w-8 h-8 rounded-full border-black border-2 "
-        onClick={onPrev}
-      >
-        {"<"}
-      </button>
-      {children}
-      <button
-        className=" m-2 w-8 h-8 rounded-full border-black border-2 "
-        onClick={onNext}
-      >
-        {">"}
-      </button>
-    </div>
-  );
-};
